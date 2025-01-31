2dca4015e065145330ddaa5db13cc5294f6f72e0
Marina Yumi Kanadani <makanadani@pm.me>
2025-01-30 23:50:20 -0300

    Fix issues

--- database.tf
+++ database.tf
@@ -5,6 +5,8 @@ resource "azurerm_mssql_server" "sql_server" {
   version                      = "12.0"
   administrator_login          = var.sql_admin_username
   administrator_login_password = var.sql_admin_password
+
+  depends_on = [ var.resource_group_name ]
 }
 
 resource "azurerm_mssql_database" "sql_database" {
--- managed-identity.tf
+++ managed-identity.tf
@@ -2,4 +2,6 @@ resource "azurerm_user_assigned_identity" "aks_identity" {
   name                = "aks-workload-identity"
   location            = var.resource_group_location
   resource_group_name = var.resource_group_name
+
+  depends_on = [ var.resource_group_name ]
 }
