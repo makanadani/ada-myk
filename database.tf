40dd09f22ff0d8737e851abb04757ed9042530af
Marina Yumi Kanadani <makanadani@pm.me>
2025-01-31 00:05:57 -0300

    Fix IP v6 to v4

--- database.tf
+++ database.tf
@@ -13,6 +13,5 @@ resource "azurerm_mssql_database" "sql_database" {
   name           = var.sql_database_name
   server_id      = azurerm_mssql_server.sql_server.id
   collation      = "SQL_Latin1_General_CP1_CI_AS"
-  max_size_gb    = 5
   sku_name       = "Basic"
 }
--- firewall.tf
+++ firewall.tf
@@ -1,6 +1,6 @@
 resource "azurerm_mssql_firewall_rule" "allow_client_ip" {
   name             = "AllowMyIP"
   server_id        = azurerm_mssql_server.sql_server.id
-  start_ip_address = "2804:7f0:b300:78a9:6820:acb7:ca4e:6b5f"
-  end_ip_address   = "2804:7f0:b300:78a9:6820:acb7:ca4e:6b5f"
+  start_ip_address = "172.212.49.139"
+  end_ip_address   = "172.212.49.139"
 }
